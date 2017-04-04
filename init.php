f23f905eb23275442a335643118c9de15e33bc5b
Nathan Toombs <nathan.toombs@classyllama.com>
2017-04-03 19:04:56 -0500

    Format files

--- init.php
+++ init.php
@@ -1,5 +1,33 @@
 <?php //0035e
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='/ioncube/ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}@dl($__ln);}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the site administrator.');exit(199);
+if (!extension_loaded('ionCube Loader')) {
+    $__oc = strtolower(substr(php_uname(), 0, 3));
+    $__ln = '/ioncube/ioncube_loader_' . $__oc . '_' . substr(phpversion(), 0, 3) . (($__oc == 'win') ? '.dll' : '.so');
+    $__oid = $__id = realpath(ini_get('extension_dir'));
+    $__here = dirname(__FILE__);
+    if (strlen($__id) > 1 && $__id[1] == ':') {
+        $__id = str_replace('\\', '/', substr($__id, 2));
+        $__here = str_replace('\\', '/', substr($__here, 2));
+    }
+    $__rd = str_repeat('/..', substr_count($__id, '/')) . $__here . '/';
+    $__i = strlen($__rd);
+    while ($__i--) {
+        if ($__rd[$__i] == '/') {
+            $__lp = substr($__rd, 0, $__i) . $__ln;
+            if (file_exists($__oid . $__lp)) {
+                $__ln = $__lp;
+                break;
+            }
+        }
+    }
+    @dl($__ln);
+} else {
+    die('The file ' . __FILE__ . " is corrupted.\n");
+}
+if (function_exists('_il_exec')) {
+    return _il_exec();
+}
+echo('Site error: the file <b>' . __FILE__ . '</b> requires the ionCube PHP Loader ' . basename($__ln) . ' to be installed by the site administrator.');
+exit(199);
 ?>
 4+oV57xDdoeI4sGD/sj/2Wcy1J/2ApDT8RiSE+ilqkfZtd8oZgJEcyit9qizh0LfBQ3UiRJG6wMB
 3Hhnu0AvgvnwChOsbRAMEHuMsDFQBMNiwBLE5Un5ocVc/8M3qOHxzhu4qYg+t+CNTe/kcOA0DLGM
